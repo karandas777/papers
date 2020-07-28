a33af84320042d2aaf3a9d9dd40145830ee794dd
karandas777 <karandas.21.08.98@gmail.com>
2020-07-28 09:53:48 +0530

    reduced col count

--- src/component/Home.js
+++ src/component/Home.js
@@ -47,7 +47,7 @@ export default class Home extends Component {
                 <div className="row m-0 p-0">
                     {
                         this.state.wallList && this.state.wallList.map((wall)=>(
-                            <div className="col-3 m-0 p-1" key={wall.id}>
+                            <div className="col-4 m-0 p-1" key={wall.id}>
                                 <a href={wall.image} download data-toggle="tooltip" data-placement="top" title="Click to download!">
                                 <img src={wall.image} className="w-100 h-100 img-fluid rounded shadow-sm" alt={wall.title} />
                                 </a>
